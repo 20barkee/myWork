4c08deac5305ff99ac83c96dc30c2d771179f17a
Ellen Barker <ellen.m.barker20@gmail.com>
2018-09-18 14:16:53 -0500

    Initials

---
+++ src/outputExamples/Initails.java
@@ -0,0 +1,14 @@
+package outputExamples;
+
+public class Initails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("EEEEEEEE     BBBBBB");
+System.out.println("E            B     B");
+System.out.println("EEEEEEEE     BBBBBB");
+System.out.println("E            B     B");
+System.out.println("EEEEEEEE     BBBBBB");
+	}
+
+}
--- src/outputExamples/NewFavoriteMovies.java
+++ src/outputExamples/NewFavoriteMovies.java
@@ -4,7 +4,7 @@ public class NewFavoriteMovies {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Favorite Movies 2");
+		System.out.println("Favorite Movies 2");
 	}
 
 }
