0ac91859da8693d775f53d61eed80cff07e3d511
Ellen Barker <ellen.m.barker20@gmail.com>
2018-09-18 08:12:46 -0500

    New Favorite Movies 2

---
+++ src/outputExamples/NewFavoriteMovies.java
@@ -0,0 +1,10 @@
+package outputExamples;
+
+public class NewFavoriteMovies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Favorite Movies 2");
+	}
+
+}
