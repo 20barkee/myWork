6576806b5fdfbb7e5dff0a5e06c4baecce367f44
Ellen Barker <ellen.m.barker20@gmail.com>
2018-10-02 08:43:16 -0500

    More cutting grass

---
+++ src/mathOperators/MoreCuttingGrass.java
@@ -0,0 +1,43 @@
+package mathOperators;
+import java.util.Scanner;
+
+public class MoreCuttingGrass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double width= -999.0;
+		double length=-999.0;
+		double mowerWidth=-999.0;
+		int mph= -999;
+		final int PAY= 12;
+		
+		Scanner input= new Scanner(System.in);
+		
+		System.out.println("What is the width of the lawn?");
+		width=input.nextDouble();
+		
+		System.out.println("What is the length of the lawn?");
+		length= input.nextDouble();
+		
+		System.out.println("What is the width of the lawn mower?");
+		mowerWidth=input.nextDouble();
+		
+		System.out.println("How many miles per hour is the lawn mower going?");
+		mph=input.nextInt();
+		
+		double area= width*length;
+		double grassCutPerHour= mowerWidth*mph;
+		double howLong= area/grassCutPerHour;
+		
+		double money= howLong*PAY;
+		
+		System.out.println("It will take "+ howLong +" to cut the lawn.");
+		System.out.println("The person will be owed $" + money + "." );
+		input.close();
+		
+
+	}
+
+}
+
