015f1b46551fcee872029c84fe3eddbab04ec743
Ellen Barker <ellen.m.barker20@gmail.com>
2018-10-03 17:27:10 -0500

    updated phone number

--- src/mathOperators/PhoneNumber.java
+++ src/mathOperators/PhoneNumber.java
@@ -7,26 +7,27 @@ public class PhoneNumber {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int areaCode =-999;
-		int middle= -999;
-		int last=-999;
+		int phoneNumber;
+		int areaCode;
+		int middleThree;
+		int lastFour;
+		int middleThree2;
 		
 		Scanner input= new Scanner(System.in);
 		
-		System.out.println("Please enter the area code of the phone number");
-		areaCode =input.nextInt();
+		System.out.println("Please enter your phone number.");
+		phoneNumber=input.nextInt();
 		
-		System.out.println("Please enter the middle three digits of the phone number");
-		middle =input.nextInt();
+		lastFour= phoneNumber % 10000;
+		areaCode= phoneNumber/ 10000000;
+		middleThree= (phoneNumber- areaCode) /10000;
+		middleThree2= middleThree % 1000;
 		
-		System.out.println("Please enter the last three digits of the phone number");
-		last =input.nextInt();
 		
-		
-		System.out.println("The complete phone number is " + areaCode+ " " + middle +" "+last);
+		System.out.println("The complete phone number is " + phoneNumber);
 		System.out.println("The area code is " + areaCode);
-		System.out.println("The middle digits are " + middle);
-		System.out.println("The last digits are " + last);
+		System.out.println("The middle digits are " + middleThree2);	
+		System.out.println("The last digits are " +lastFour);
 		
 		input.close();
 		
