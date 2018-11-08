f8a928ca2d24f642d14c9943507f36d340968435
Ellen Barker <ellen.m.barker20@gmail.com>
2018-11-08 07:59:37 -0600

    Fixed Countby5's

--- src/loops/Countby5.java
+++ src/loops/Countby5.java
@@ -5,9 +5,12 @@ public class Countby5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int num=0;
-		while(num<47)
+System.out.println("This program will count by 5's to display every number less than 49");
+		while(num<45)
 		{
+			
 			num=num+5;
+			
 			System.out.println(num);
 		}
 		System.out.println("The end.");
