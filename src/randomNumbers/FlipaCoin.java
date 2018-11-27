5decd9e27031e93e13cb8e0a163ab5d041853502
Ellie Barker <ellen.m.barker20@gmail.com>
2018-11-27 09:04:21 -0600

    changes

--- src/randomNumbers/FlipaCoin.java
+++ src/randomNumbers/FlipaCoin.java
@@ -8,39 +8,33 @@ public class FlipaCoin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	Random generator=new Random();
-		int min=1;
-		int max=2;
-		int heads=0;
-		int tails=0;
-		int randomNum=0;
-		double largest=0;
-		double smallest=0;
-		for(int j=0; j<1000; j++) {
-for(int i=0; i<100000; i++)
-{
-	randomNum=min+generator.nextInt(max-min+1);
-	if(randomNum==0)
-	{
-		heads++;
-	}
-}
-double headsPer=100.0*(heads/100.0);
+		Random generator = new Random();
+		int min = 1;
+		int max = 2;
+		int heads = 0;
+		int randomNum = 0;
+		double largest = 0;
+		double smallest = 100;
+		for (int j = 0; j < 1000; j++) {
+			for (int i = 0; i < 100000; i++) {
+				randomNum = min + generator.nextInt(max - min + 1);
+				if (randomNum == 1) {
+					heads++;
+				}
+			}
+			double headsPer = 100 * (heads / 100000.0);
 //makes the number a percent
 
-	if(headsPer>largest) {
-		largest=heads;
-	}
-	if(headsPer<smallest) {
-		smallest=heads;
-	}
-	heads=0;
+			if (headsPer > largest) {
+				largest = headsPer;
+			}
+			if (headsPer < smallest) {
+				smallest = headsPer;
+			}
+			heads = 0;
 		}
-System.out.println(largest);
-System.out.println(smallest);
+		System.out.println(largest + " percent");
+		System.out.println(smallest + " percent");
 
-
-}
 	}
-
-
+}
