e945743b8e46c38f76a99abc8be67fadc4c2ac0b
Ellie Barker <ellen.m.barker20@gmail.com>
2019-01-14 22:11:56 -0600

    changes

--- src/ArrayLists/ForEachArrayList.java
+++ src/ArrayLists/ForEachArrayList.java
@@ -1,10 +1,30 @@
 package ArrayLists;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ForEachArrayList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner input= new Scanner(System.in);
 
+System.out.println("How many employees are in the company?");
+double numEmployees=input.nextDouble();
+
+		ArrayList <double>wage= new ArrayList<double>();
+		
+
+		for(int i=0; i<numEmployees;i++) {
+			System.out.println("Enter in the age of the student");
+			wages.aff(input.nextDouble);
+			
+		}
+
+		for(int temp:ages) {
+			sum+=temp;
+		}
+
+		System.out.println("Average: " + (sum/userinput));
 	}
 
 }
